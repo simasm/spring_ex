0d91b3a9322aa15d97f5806f3c52ef9a69253462
simasm <simonas.mark@gmail.com>
2021-12-15 18:50:29 +0200

    more ex

---
+++ hello-world-calc/src/main/resources/public/precache-manifest.c06c0336770a4dd4f5afddc0e4c7db37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca24d49df2f3ce46fc464baf6eb1a31f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64730c6fdb2cac8c24dc",
+    "url": "/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "6026e0eee97f8777d1ae",
+    "url": "/static/css/main.e7512b03.chunk.css"
+  },
+  {
+    "revision": "64730c6fdb2cac8c24dc",
+    "url": "/static/js/2.463b41aa.chunk.js"
+  },
+  {
+    "revision": "b0d2420da847bbc9cc10dc274ffdeef6",
+    "url": "/static/js/2.463b41aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38154d7d81c0027ea7fa",
+    "url": "/static/js/3.1625562b.chunk.js"
+  },
+  {
+    "revision": "6026e0eee97f8777d1ae",
+    "url": "/static/js/main.8aae4882.chunk.js"
+  },
+  {
+    "revision": "8d4f642fed22a52068d7",
+    "url": "/static/js/runtime-main.c3144509.js"
+  }
+]);
